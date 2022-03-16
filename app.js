b272861129ad810e42c426965b6db6acca40a4ce
Dinujaya-Sandaruwan <itkindom.dhomes@gmail.com>
2022-03-16 07:52:11 +0530

    3.15 Done

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 // console.log("saman")
 
-// Get date and time
+var late_function = (function() {
+    var executed = false;
+    return function() {
+        if (!executed) {
+            executed = true;
+            back_to = 'true';
+        }
+    };
+})();
+late_function();
 
-let today = new Date();
 
-let hours = addZero(today.getHours());
-let minutes = addZero(today.getMinutes());
-let seconds = addZero(today.getSeconds());
-let current_time = `${hours}:${minutes}:${seconds}`;
-// output.innerText = current_time;
-
-function addZero(num){
-    return num < 10 ? `0${num}`:num;
-}
 
 //Dict Keypress
 
@@ -33,7 +32,28 @@ document.addEventListener("keydown", (e) => {
         updateLetters(keypress)
     }
     else if (e.key == 'Enter') {
+        document.getElementById('screen_server').style.display = 'none';
+        document.getElementById('after_scan_main').style.display = 'flex';
         submitGuess();
+
+        if(back_to == 'true'){
+            var back_to_screen_01 = setTimeout(back_to_screen_server, 5000);
+            clearTimeout(back_to_screen_02);
+            back_to = 'false'
+            function back_to_screen_server() {
+                document.getElementById('screen_server').style.display = 'flex';
+                document.getElementById('after_scan_main').style.display = 'none';
+            }
+        }
+        else{
+            var back_to_screen_02 = setTimeout(back_to_screen_server, 5000);
+            clearTimeout(back_to_screen_01);
+            back_to = 'true'
+            function back_to_screen_server() {
+                document.getElementById('screen_server').style.display = 'flex';
+                document.getElementById('after_scan_main').style.display = 'none';
+            }
+        }
     }
 
 
@@ -43,6 +63,7 @@ const updateTiles = () => {
 }
 
 });
+
 
 // Update Letters
 const updateLetters = (letter) => {
@@ -58,24 +79,58 @@ const submitGuess = () =>{
     console.log('Enter has been pressed')
     // Change Name
     var main_name = document.getElementById("name");
+    var st_avater = document.getElementById("avater");
     if (currentGuess.dataset.letters == '24108') {
         main_name.innerHTML = "Dinujaya Sandaruwan";
+        st_avater.src="img/me.jpeg";
         document.cookie = "dinujaya=true;";
     }
     if (currentGuess.dataset.letters == '24109') {
         main_name.innerHTML = "Heshani Mahinsala";
+        st_avater.src="img/dummy/heshani.jpg";
         document.cookie = "heshani=true;";
     }
+    if (currentGuess.dataset.letters == '24110') {
+        main_name.innerHTML = "Saman Kumara";
+        st_avater.src="img/dummy/1.jpg";
+        document.cookie = "heshani=true;";
+    }
+    if (currentGuess.dataset.letters == '24111') {
+        main_name.innerHTML = "Dinesh Gamage";
+        st_avater.src="img/dummy/2.jpg";
+        document.cookie = "heshani=true;";
+    }
+    if (currentGuess.dataset.letters == '24112') {
+        main_name.innerHTML = "Sirimath Sugathapala";
+        st_avater.src="img/dummy/3.jpg";
+        document.cookie = "heshani=true;";
+    }
+    if (currentGuess.dataset.letters == '24113') {
+        main_name.innerHTML = "Wasantha Kapuge";
+        st_avater.src="img/dummy/4.jpg";
+        document.cookie = "heshani=true;";
+    }
+
+    // Get date and time
+    let today = new Date();
+
+    let hours = addZero(today.getHours());
+    let minutes = addZero(today.getMinutes());
+    let seconds = addZero(today.getSeconds());
+    let current_time = `${hours}:${minutes}:${seconds}`;
+    // output.innerText = current_time;
+
+    function addZero(num){
+        return num < 10 ? `0${num}`:num;
+    }
+
     //Change Time
     came_time = document.getElementById('came_time');
     came_time.innerHTML = 'Came to school at :- ' + current_time + ' A.M';
     // main_name.innerText = currentGuess.dataset.letters;
     currentGuess.dataset.letters =''
     // alert("Your Number is" + currentGuess.dataset.letters);
-
-
 }
-
 
 // document.cookie = "dinujaya=true;";
 
