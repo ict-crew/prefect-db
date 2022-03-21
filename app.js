5759ee06225f539c1c0534191222b05455cbc3d5
Dinujaya-Sandaruwan <itkindom.dhomes@gmail.com>
2022-03-22 01:25:57 +0530

    Process Bar Done

--- app.js
+++ app.js
@@ -30,9 +30,8 @@ document.addEventListener("keydown", (e) => {
     }
     else if (e.key == 'Enter') {
         submitGuess();
-        document.getElementById('screen_server').style.display = 'none';
-        document.getElementById('after_scan_main').style.display = 'flex';
-        
+        // show_student_details()
+       
 
         if (back_to == 'true') {
             back_to_screen1 = setTimeout(back_to_screen_server, 10000);
@@ -69,6 +68,11 @@ const updateLetters = (letter) => {
     console.log('Updated currentGuess.dataset.letters = ' + currentGuess.dataset.letters);
 
 }
+// This function will show data element when clicked enter
+function show_student_details() {
+    document.getElementById('screen_server').style.display = 'none';
+    document.getElementById('after_scan_main').style.display = 'flex';
+}
 
 const submitGuess = () =>{
     console.log('Enter has been pressed')
@@ -83,34 +87,44 @@ const submitGuess = () =>{
     var main_name = document.getElementById("name");
     var st_avater = document.getElementById("avater");
     if (currentGuess.dataset.letters == '24108') {
+        show_student_details()
         main_name.innerHTML = "Dinujaya Sandaruwan";
         st_avater.src="img/me.jpeg";
         document.cookie = "24108=24108;";
     }
     if (currentGuess.dataset.letters == '24109') {
+        show_student_details()
         main_name.innerHTML = "Heshani Mahinsala";
         st_avater.src="img/dummy/heshani.jpg";
         document.cookie = "24109=24109;";
     }
     if (currentGuess.dataset.letters == '24110') {
+        show_student_details()
         main_name.innerHTML = "Saman Kumara";
         st_avater.src="img/dummy/1.jpg";
         document.cookie = "24110=24110;";
     }
     if (currentGuess.dataset.letters == '24111') {
+        show_student_details()
         main_name.innerHTML = "Dinesh Gamage";
         st_avater.src="img/dummy/2.jpg";
         document.cookie = "24111=24111;";
     }
     if (currentGuess.dataset.letters == '24112') {
+        show_student_details()
         main_name.innerHTML = "Sirimath Sugathapala";
         st_avater.src="img/dummy/3.jpg";
         document.cookie = "24112=24112;";
     }
     if (currentGuess.dataset.letters == '24113') {
+        show_student_details()
         main_name.innerHTML = "Wasantha Kapuge";
         st_avater.src="img/dummy/4.jpg";
         document.cookie = "24113=24113;";
+    }
+    if (currentGuess.dataset.letters == '6604') {
+        start_process_bar()
+        document.cookie = "start_server=server_started;";
     }
 
     // Get date and time
@@ -177,4 +191,28 @@ function getCookie(name) {
         }
     }
     return "";
+}
+
+
+//upload Data to Server 
+
+function start_process_bar() {
+    document.getElementById('screen_server').style.display = 'none';
+    document.getElementById('data_upload_main').style.display = "flex";
+
+    const progressBar = document.getElementsByClassName('progress-bar')[0]
+    setInterval(() => {
+        const computedStyle = getComputedStyle(progressBar)
+        const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
+        progressBar.style.setProperty('--width', width + .1)
+    }, 5)
+
+    setTimeout(display_sucess_message, 5000);
+    function display_sucess_message() {
+        clearTimeout(back_to_screen1);
+        document.getElementById('data_upload_test').style.display = "block";
+        document.getElementById('server_down').style.display = "block";
+        document.body.style.backgroundImage = "url('img/bg-black-white.jpg')";
+    }
+    
 }
